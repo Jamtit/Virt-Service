371205cdfcb4e65ae072ddf8d5c992ee5d65dc19
uncanny <uncanny@lc-rp.mp>
2022-12-10 21:02:14 +0200

    web

---
+++ server.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    
+    <div class="main_div">
+        <div class="box">
+            <h2>Server: 1</h2>
+            <p>Type: Debian <br>GB:5</p>
+            <!-- <button id="create">Create</button> -->
+            <a id="create" href="users.php">Create</a>
+        </div>
+        <div class="box">
+            <h2>Server: 2</h2>
+            <p>Type: Debian<br>GB:10</p>
+            <a id="create" href="users.php">Create</a>
+        </div>
+        <div class="box">
+            <h2>Server: 3</h2>
+            <p>Type: Ubuntu<br>GB:15</p>
+            <a id="create" href="users.php">Create</a>
+        </div>
+    </div>
+</body>
+</html>
---
+++ users.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Phone page</title>
+</head>
+<body>
+<div style="width: 60%; height:500px; background-color: #fbfff0; margin: 0 auto!important; display: grid; grid-template-columns: 1fr 1fr; font-family: Arial, Helvetica, sans-serif;">
+<div style="display:flex; justify-content: center; align-items:center">Person</div>
+<div style="display:flex; justify-content: center; align-items:center">Number</div>
+<?php
+
+include 'vars.php';
+
+$conn = pg_connect("host=10.0.0.169 port=5432 dbname=postgres user=postgres password=1234");
+if (!$conn) {
+  echo "An error occurred.\n";
+  exit;
+}
+
+$result = pg_query($conn, "SELECT * FROM data");
+if (!$result) {
+  echo "An error occurred.\n";
+  exit;
+}
+
+while ($row = pg_fetch_row($result)) {
+    $div1 = '<div style="display:flex; justify-content: center; align-items:center">' . $row[0] . '</div>';
+
+    $div2 = '<div style="display:flex; justify-content: center; align-items:center">';
+    $div2 .= '<a href="https://api.whatsapp.com/send?phone='. $row[1] .'">'. $row[1] .'</a></div>';
+    
+
+    echo $div1;
+    echo $div2;
+  }
+?> 
+</div>
+
+</body>
+</html>
