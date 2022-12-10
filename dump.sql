16262c939e5287b715836d71d9a14866fc0116d0
uncanny <uncanny@lc-rp.mp>
2022-12-10 23:52:38 +0200

    dump

---
+++ dump.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS data (
+  name varchar(255),
+  pass varchar(32) PRIMARY KEY
+  );
+    INSERT INTO data VALUES('tima7442', 'zoqqB2++');
+    INSERT INTO data VALUES('dima7574', 'L1NUX+itmoksl123');
+ALTER TABLE data OWNER TO "postgres";
